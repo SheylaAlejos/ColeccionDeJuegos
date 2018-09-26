09b8eb88b7df6adccbef4328eee9c5dae82edeca
Sheyla Alejos <sheyla.alejos@tecsup.edu.pe>
2018-09-26 16:27:40 -0500

    hasta agregar galeria.

---
+++ ColeccionDeJuegos/JuegoViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class JuegoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var JuegoImagenView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        
+    }
+
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    let imagenSeleccionada = info[UIImagePickerControllerOriginalImage] as! UIImage
+        JuegoImagenView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = UIImagePNGRepresentation(JuegoImagenView.image!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+        
+        
+    }
+    
+
+
+}
